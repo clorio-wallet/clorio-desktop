eb7562a235369a94b086c3c543eba5cd6471aa9c
Cryptosaka <79313706+Cryptosaka@users.noreply.github.com>
2025-01-07 11:50:20 -0800

    Added persistent state

--- packages/renderer/src/store/privacy.ts
+++ packages/renderer/src/store/privacy.ts
@@ -1,14 +1,29 @@
 import {atom} from 'recoil';
+import {loadState, saveState} from './localStorage';
 
 export interface IPrivacyMode {
   active: boolean;
   fields: string[];
 }
 
+const PERSISTENCE_KEY = 'privacyModeState';
+
 export const privacyModeState = atom<IPrivacyMode>({
-  key: 'privacyModeState',
+  key: PERSISTENCE_KEY,
   default: {
     active: false,
     fields: ['balance'],
   },
+  effects_UNSTABLE: [
+    ({setSelf, onSet}) => {
+      const savedState = loadState(PERSISTENCE_KEY);
+      if (savedState !== undefined) {
+        setSelf(savedState);
+      }
+
+      onSet(newState => {
+        saveState(PERSISTENCE_KEY, newState);
+      });
+    },
+  ],
 });
